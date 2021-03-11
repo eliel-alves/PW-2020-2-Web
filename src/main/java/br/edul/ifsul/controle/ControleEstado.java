3411e7e79ef1af1e54d1b910718a96f06f3cf2ec
eliel <eliel@ELIEL-Notebook>
2021-03-11 08:47:50 -0300

    Commit inicial.

---
+++ src/main/java/br/edul/ifsul/controle/ControleEstado.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edul.ifsul.controle;
+
+import br.edu.ifsul.dao.EstadoDAO;
+import br.edu.ifsul.modelo.Estado;
+import br.edu.ifsul.util.Util;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author eliel
+ */
+@Named(value = "controleEstado")
+@ViewScoped
+public class ControleEstado implements Serializable {
+    
+    @EJB
+    private EstadoDAO<Estado> dao;
+    private Estado objeto;
+    
+    public ControleEstado(){
+        
+    }
+    
+    public String listar(){
+        return "/privado/estado/listar?faces-redirect=true";
+    }
+    
+    public void novo(){
+        objeto = new Estado();
+    }
+    
+    public void alterar(Object id){
+        try {
+            objeto = dao.getObjectByID(id);
+        } catch (Exception e) {
+            Util.mensagemInformacao("Erro ao recuperar o objeto: " + Util.getMensagemErro(e));
+        }
+    }
+    
+    public void excluir(Object id){
+        try {
+            objeto = dao.getObjectByID(id);
+            dao.remove(objeto);
+            Util.mensagemInformacao("Objeto removido com sucesso!");
+        } catch (Exception e) {
+            Util.mensagemInformacao("Erro ao remover o objeto: " + Util.getMensagemErro(e));
+        }
+    }
+    
+    public void salvar(){
+        try {
+            if(objeto.getId() == null){
+                dao.persist(objeto);
+            }else{
+                dao.merge(objeto);
+            }
+            Util.mensagemInformacao("Objeto persistido com sucesso!");
+        } catch (Exception e) {
+            Util.mensagemInformacao("Erro ao salvar o objeto: " + Util.getMensagemErro(e));
+        }
+    }
+
+    public EstadoDAO<Estado> getDao() {
+        return dao;
+    }
+
+    public void setDao(EstadoDAO<Estado> dao) {
+        this.dao = dao;
+    }
+
+    public Estado getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(Estado objeto) {
+        this.objeto = objeto;
+    }
+    
+    
+    
+}
